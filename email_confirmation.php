38108cee7e9d7020c6f00b633467f67df9689286
Emrysisata <128702841+Emrysisata@users.noreply.github.com>
2024-07-17 17:30:43 +0100

    Add files via upload

---
+++ email_confirmation.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+$code = $_POST["code"];
+$email = $_SESSION["email"];
+
+// These are info needed to connect to the database
+$databaseServerName = "localhost";
+$databaseUserName = "root";
+$databaseUserPassword = "";
+$dbname = "mammy_koker";
+
+
+//These code below is use to create connection to our users database
+$connection = mysqli_connect($databaseServerName, $databaseUserName, $databaseUserPassword)
+
+//the code below is use to check if we have successfull connected to the database
+if (!$connection){
+    die("Cannot connect to the database" . mysqli_connection _error);
+ 
+ }else {
+    $sql = "SELECT OTP FROM otpholder WHERE EMAIL ='" . $email . "'";
+
+    $fetch_query = mysqli_query($connection, $sql);
+
+    if ($code in $fetch_query){
+        //take the data that has the primary key $email from the TEMPORAL_USER table to the user USER
+        $sql = "SELECT * FROM temporaluser WHERE EMAIL ='" . $email . "'";
+        $query_result = mysqli_query($connection, $sql);
+
+        //setting the user login details on the user table
+        $putting_sql = "INSERT INTO user VALUES('" . $query_result[0] ."','"$$query_result[1] ."','". $$query_result[2] ."', '"$$query_result[3] ."', '"$$query_result[4] ."',"$$query_result[5] . "')";
+        $query_result_2 = mysqli_query($connection, $putting_sql);
+        //and redirect the user to the login page
+        header('Location: https://mammy-koker/login.html');
+        exit;
+    } else {
+        echo "invalid one time code";
+        //redirect you to the email confirmation page so that you can confirm your email
+        header('Location: https://mammy-koker/email_confirmation.html');
+        exit;
+    }
+
+ }
+?>
--- login.php
+++ login.php
@@ -3,53 +3,75 @@ session_start();
 //getting login details entered
 $email = $_POST["email"];
 $password = $_POST["password"];
-$remember_me_flag = $_POST["remember_me"];
+//$remember_me_flag = $_POST["remember_me"];
 
 
 // These are info needed to connect to the database
-$databaseServerName = "";
-$databaseUserName = "";
+
+
+$databaseServerName = "localhost";
+$databaseUserName = "root";
 $databaseUserPassword = "";
+$dbname = "mammy_koker";
+
 
 //These code below is use to create connection to our users database
-$connection = mysqli_connect($databaseServerName, $databaseUserName, $databaseUserPassword)
+
+$connection = mysqli_connect($databaseServerName, $databaseUserName, $databaseUserPassword, $dbname);
+
+//echo strval($connection);
 
 //the code below is use to check if we have successfull connected to the database
 if (!$connection){
-    die("Cannot connect to the database" . mysqli_connection _error);
+    die("Cannot connect to the database" . mysqli_error);
  
  } else{
 
-    $sql = "SELECT EMAIL FROM credential WHERE EMAIL =" . $email;
+   //echo mysqli_query($connection, "SELECT * FROM credential");
+
+    $sql = "SELECT EMAIL FROM credential WHERE EMAIL ='" . $email . "'";
 
     $fetch_query = mysqli_query($connection, $sql);
+   
+   
+   
+   // print $fetch_query;
     
     //trying to see if the user has credential already on the database 
-    echo $fetch_query;
+    //echo $fetch_query;
+
+    if ($fetch_query == ""){
+      
+      echo "no account found for this Email:" . $email;
+      
+    }
 
     if ($fetch_query != ""){
-      $sql = "SELECT PASSWORD FROM credential WHERE EMAIL =" . $email;
+       
+      $sql = "SELECT PASSWORD FROM credential WHERE EMAIL ='" . $email . "'";
 
       if (mysqli_query($connection, $sql) == $password){
-         //the user login credent
-          ial is correct
-         //redirecting user to the personalized home page
+         //the user login credential
+         //checking if the user wants the device to remember his/her login details, so that the sign in process will be automatically done for them
+          //Note: the system will only remember you for 30 days, after 30 days, you will need to login in again
+          if ($remember_me_flag != ""){
+            setcookie("email", $email, (30 * 86400), "/");
+            setcookie("password", $password, (30 * 86400), "/");
+        }
+
          $_SESSION["email"] = $email;
 
-          //checking if the user wants the device to remember his/her login details, so that the sign in process will be automatically done for them
-          //Note: the system will only remember you for 30 days, after 30 days, you will need to sign in again
-          if ($remember_me_flag != ""){
-              setcookie("email", $email, (30 * 86400), "/");
-              setcookie("password", $password, (30 * 86400), "/");
-          }
-         
-         header('Location: https://mammy-koker/home.html');
+         //redirecting user to the personalized home page
+         header('Location: https://mammykoker/home.html');
          exit;
-      }else {
-         echo "USER LOGIN PASSWORD IS INCORRECT";
-      }else {
-         echo "No account found for the user email";
+          
+         
+        
       }
+      else {
+         echo "USER LOGIN PASSWORD IS INCORRECT";
+      }
+      
 
     }
 
